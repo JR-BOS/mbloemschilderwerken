cf23d004d257900133e8ceb81569422824edf8bc
Jesse-Bos <jesse_bos@live.com>
2021-05-11 21:02:43 +0200

    navbar

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 $(document).ready(function () {
+  //NAVBAR SCROLL
   $(window).scroll(function () {
+    const isCollapsed = document.querySelector(".navbar-collapse.show");
     if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
     } else {
-      $(".navbar").removeClass("sticky");
+      if (!isCollapsed) {
+        $(".navbar").removeClass("sticky");
+      }
     }
   });
 });
+//END NAVBAR SCROLL
 
+//ANDERE NAVBAR SHIT
 const toggler = document.querySelector(".navbar-toggler");
 const collapse = document.querySelector(".navbar-collapse");
+const navbar = document.querySelector(".navbar");
+
+toggler.addEventListener("click", function () {
+  if (navbar.classList.contains("test")) {
+    $(".navbar").removeClass("test");
+  } else {
+    $(".navbar").addClass("sticky");
+    $(".navbar").addClass("test");
+  }
+});
 
 collapse.addEventListener("click", (e) => {
   const isCollapsed = document.querySelector(".navbar-collapse.show");
